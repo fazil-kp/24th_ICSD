84efa260deddede6ca0cc26ddc0e0ade1a555c6c
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-12-31 21:36:22 +0530

    update colors.dart

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 extension ThemeContext on BuildContext {
   Color get primary => Theme.of(this).colorScheme.primary;
-  Color get background => Theme.of(this).colorScheme.background;
+  Color get background => Theme.of(this).colorScheme.surface;
   Color get surface => Theme.of(this).colorScheme.surface;
-  Color get onBackground => Theme.of(this).colorScheme.onBackground;
+  Color get onBackground => Theme.of(this).colorScheme.onSurface;
   Color get onPrimary => Theme.of(this).colorScheme.onPrimary;
 }
 
@@ -22,8 +22,6 @@ const lightColorScheme = ColorScheme(
   onTertiary: Color(0xFF000000),
   tertiaryContainer: Color(0xFFF7D8FF),
   onTertiaryContainer: Color.fromARGB(255, 0, 0, 0),
-  background: Color(0xffF5F5F5),
-  onBackground: Color.fromARGB(255, 0, 0, 0),
   surface: Color.fromARGB(255, 230, 233, 232),
   onSurface: Color.fromARGB(255, 0, 0, 0),
   error: Color(0xFFEA1505),
@@ -34,7 +32,7 @@ const lightColorScheme = ColorScheme(
   inversePrimary: Color(0xFF005FAF),
   inverseSurface: Color(0xFFE3E2E6),
   onInverseSurface: Color.fromARGB(255, 0, 0, 0),
-  surfaceVariant: Color.fromARGB(37, 0, 0, 0),
+  surfaceContainerHighest: Color.fromARGB(37, 0, 0, 0),
   shadow: Color.fromARGB(255, 0, 0, 0),
   surfaceTint: Color(0xFFE3E2E6),
   outline: Color(0xFFE3E2E6),
@@ -57,8 +55,6 @@ const darkColorScheme = ColorScheme(
   onTertiary: Color.fromARGB(255, 182, 181, 181),
   tertiaryContainer: Color(0xFF553F5D),
   onTertiaryContainer: Color(0xFFF7D8FF),
-  background: Color(0xFF131313),
-  onBackground: Colors.black,
   surface: Color(0xFF313640),
   onSurface: Color.fromARGB(255, 211, 196, 255),
   surfaceTint: Color(0xFFFFFFFF),
@@ -66,7 +62,7 @@ const darkColorScheme = ColorScheme(
   onError: Color(0xFFE8E5E5),
   errorContainer: Color(0xFFFFB70A),
   onErrorContainer: Color(0xFFB00020),
-  surfaceVariant: Color(0x26FFFFFF),
+  surfaceContainerHighest: Color(0x26FFFFFF),
   onSurfaceVariant: Color(0xFFC3C6CF),
   inverseSurface: Color(0xFFE3E2E6),
   onInverseSurface: Color(0xFF1A1C1E),
