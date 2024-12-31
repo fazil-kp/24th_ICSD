b29cdb34915ff2725f57def9abaf0d599b703f7e
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-12-31 22:21:22 +0530

    update 10 files

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:icsd/config/colors.dart';
 import 'package:icsd/config/theme.dart';
@@ -50,6 +51,32 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              10.width,
+              Expanded(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 150.0,
+                    autoPlay: true,
+                    aspectRatio: 1.0,
+                    viewportFraction: 1,
+                  ),
+                  items: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Container(
+                        width: context.width(),
+                        decoration: BoxDecoration(color: context.secondary, borderRadius: BorderRadius.circular(12)),
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            Text("Welcome to", style: context.bodySmall?.copyWith(fontSize: 12, fontWeight: FontWeight.w500, color: context.surface)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ],
