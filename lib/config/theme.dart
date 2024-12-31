ca9a04c536e60c7527c94764820eb2318782f655
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-12-31 21:35:28 +0530

    update theme.dart

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -25,9 +25,9 @@ extension CopyWithTextTheme on TextTheme {
     String fontFamily = "Inter";
 
     return copyWith(
-      bodySmall: bodySmall?.copyWith(color: colorScheme?.onBackground ?? color, fontFamily: fontFamily, fontSize: 12),
-      bodyMedium: bodyMedium?.copyWith(color: colorScheme?.onBackground ?? color, fontFamily: fontFamily, fontSize: 14),
-      bodyLarge: bodyLarge?.copyWith(color: colorScheme?.onBackground ?? color, fontFamily: fontFamily, fontSize: 16),
+      bodySmall: bodySmall?.copyWith(color: colorScheme?.onSurface ?? color, fontFamily: fontFamily, fontSize: 12),
+      bodyMedium: bodyMedium?.copyWith(color: colorScheme?.onSurface ?? color, fontFamily: fontFamily, fontSize: 14),
+      bodyLarge: bodyLarge?.copyWith(color: colorScheme?.onSurface ?? color, fontFamily: fontFamily, fontSize: 16),
     );
   }
 }
@@ -67,47 +67,47 @@ class AppTheme {
       brightness: data.brightness,
       useMaterial3: true,
       tooltipTheme: TooltipThemeData(textStyle: data.textTheme.bodySmall?.copyWith(color: Colors.white), decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8))),
-      textSelectionTheme: TextSelectionThemeData(cursorColor: data.colorScheme.primary, selectionColor: data.colorScheme.primary.withOpacity(0.5)),
-      dialogBackgroundColor: data.colorScheme.background,
+      textSelectionTheme: TextSelectionThemeData(cursorColor: data.colorScheme.primary, selectionColor: data.colorScheme.primary),
+      dialogBackgroundColor: data.colorScheme.surface,
       colorScheme: data.colorScheme,
       typography: _typography,
       iconButtonTheme: IconButtonThemeData(style: IconButton.styleFrom(iconSize: 30, foregroundColor: data.colorScheme.primary)),
       checkboxTheme: CheckboxThemeData(side: const BorderSide(color: Color(0XFF757474)), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3))),
-      iconTheme: IconThemeData(color: data.colorScheme.onBackground, size: 30),
+      iconTheme: IconThemeData(color: data.colorScheme.onSurface, size: 30),
       cardTheme: CardTheme(surfaceTintColor: data.colorScheme.surface, color: data.colorScheme.surface, margin: const EdgeInsets.all(8), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-      expansionTileTheme: ExpansionTileThemeData(backgroundColor: data.colorScheme.background, collapsedBackgroundColor: data.colorScheme.background),
+      expansionTileTheme: ExpansionTileThemeData(backgroundColor: data.colorScheme.surface, collapsedBackgroundColor: data.colorScheme.surface),
       elevatedButtonTheme: _buttonStyle(data.colorScheme, data.textTheme),
       outlinedButtonTheme: _outlineStyle(data.colorScheme, data.textTheme),
       cardColor: data.colorScheme.surface,
-      listTileTheme: ListTileThemeData(tileColor: data.colorScheme.background, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+      listTileTheme: ListTileThemeData(tileColor: data.colorScheme.surface, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
       scrollbarTheme: ScrollbarThemeData(
-        thickness: MaterialStateProperty.resolveWith((states) {
-          if (states.contains(MaterialState.pressed)) {
+        thickness: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) {
             return 5;
           }
           return 5;
         }),
-        thumbColor: MaterialStateProperty.resolveWith((states) {
-          if (states.contains(MaterialState.pressed)) {
+        thumbColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) {
             return data.colorScheme.primary;
           }
           return data.colorScheme.primary;
         }),
       ),
-      inputDecorationTheme: InputDecorationTheme(suffixIconColor: data.colorScheme.primary, floatingLabelBehavior: FloatingLabelBehavior.always, contentPadding: const EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12), isDense: true, filled: true, fillColor: data.colorScheme.inversePrimary.withOpacity(0.1), errorStyle: data.textTheme.bodySmall?.copyWith(color: data.colorScheme.error), focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8)), prefixIconColor: data.colorScheme.outline, enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8)), labelStyle: data.textTheme.bodyMedium?.copyWith(color: data.colorScheme.secondary), border: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8))),
+      inputDecorationTheme: InputDecorationTheme(suffixIconColor: data.colorScheme.primary, floatingLabelBehavior: FloatingLabelBehavior.always, contentPadding: const EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12), isDense: true, filled: true, fillColor: data.colorScheme.inversePrimary, errorStyle: data.textTheme.bodySmall?.copyWith(color: data.colorScheme.error), focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8)), prefixIconColor: data.colorScheme.outline, enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8)), labelStyle: data.textTheme.bodyMedium?.copyWith(color: data.colorScheme.secondary), border: OutlineInputBorder(borderSide: BorderSide(width: 0.7, color: data.colorScheme.tertiary), gapPadding: 0, borderRadius: BorderRadius.circular(8))),
       textTheme: data.textTheme,
-      dialogTheme: DialogTheme(backgroundColor: data.colorScheme.background, elevation: 1, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)), contentTextStyle: data.textTheme.bodySmall, titleTextStyle: data.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)),
+      dialogTheme: DialogTheme(backgroundColor: data.colorScheme.surface, elevation: 1, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)), contentTextStyle: data.textTheme.bodySmall, titleTextStyle: data.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)),
       drawerTheme: DrawerThemeData(backgroundColor: data.colorScheme.primary, shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(30), topRight: Radius.circular(30)))),
-      dividerTheme: DividerThemeData(thickness: 0.8, color: data.colorScheme.outline.withOpacity(.3)),
+      dividerTheme: DividerThemeData(thickness: 0.8, color: data.colorScheme.outline),
       visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
-      scaffoldBackgroundColor: data.colorScheme.background,
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: data.colorScheme.background, selectedIconTheme: IconThemeData(color: data.colorScheme.secondary, size: 30), unselectedIconTheme: IconThemeData(color: data.colorScheme.outline, size: 25), selectedLabelStyle: const TextStyle(color: Colors.green), selectedItemColor: data.colorScheme.secondary, showUnselectedLabels: true, showSelectedLabels: false, unselectedItemColor: data.colorScheme.outline, unselectedLabelStyle: const TextStyle(fontSize: 10), type: BottomNavigationBarType.fixed),
+      scaffoldBackgroundColor: data.colorScheme.surface,
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: data.colorScheme.surface, selectedIconTheme: IconThemeData(color: data.colorScheme.secondary, size: 30), unselectedIconTheme: IconThemeData(color: data.colorScheme.outline, size: 25), selectedLabelStyle: const TextStyle(color: Colors.green), selectedItemColor: data.colorScheme.secondary, showUnselectedLabels: true, showSelectedLabels: false, unselectedItemColor: data.colorScheme.outline, unselectedLabelStyle: const TextStyle(fontSize: 10), type: BottomNavigationBarType.fixed),
       navigationBarTheme: const NavigationBarThemeData(backgroundColor: Colors.red, indicatorColor: Colors.red),
     );
   }
 
   static ThemeData reverseThemeData(ThemeData data) {
-    return data.copyWith(textTheme: data.textTheme.copyWithTextTheme(color: data.colorScheme.onSurfaceVariant), colorScheme: data.colorScheme.copyWith(background: data.colorScheme.surfaceVariant, onBackground: data.colorScheme.onSurfaceVariant), scaffoldBackgroundColor: data.colorScheme.surfaceVariant, inputDecorationTheme: data.inputDecorationTheme.copyWith(filled: true));
+    return data.copyWith(textTheme: data.textTheme.copyWithTextTheme(color: data.colorScheme.onSurfaceVariant), colorScheme: data.colorScheme.copyWith(surface: data.colorScheme.surfaceContainerHighest, onSurface: data.colorScheme.onSurfaceVariant), scaffoldBackgroundColor: data.colorScheme.surfaceContainerHighest, inputDecorationTheme: data.inputDecorationTheme.copyWith(filled: true));
   }
 
   static ThemeData gradientTheme(ThemeData data) {
