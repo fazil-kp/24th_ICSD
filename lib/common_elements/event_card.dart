9b1c9cf19a3798e49888d5e4eac657faca55ec28
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-01-01 23:37:05 +0530

    update event_card.dart

--- lib/common_elements/event_card.dart
+++ lib/common_elements/event_card.dart
@@ -3,7 +3,6 @@ import 'package:icsd/config/colors.dart';
 import 'package:icsd/config/theme.dart';
 import 'package:mine/config/color/colors.dart';
 import 'package:mine/extension/context_extension.dart';
-import 'package:mine/extension/integer_extension.dart';
 
 class EventCard extends StatelessWidget {
   final String image;
@@ -22,26 +21,20 @@ class EventCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
+        spacing: 5,
         children: [
-          Container(
-            height: 100,
-            width: context.width(),
-            decoration: BoxDecoration(color: whiteColor, borderRadius: BorderRadius.circular(12), image: DecorationImage(image: AssetImage(image), fit: BoxFit.fill)),
-          ),
-          5.height,
+          Container(height: 100, width: context.width(), decoration: BoxDecoration(color: whiteColor, borderRadius: BorderRadius.circular(12), image: DecorationImage(image: AssetImage(image), fit: BoxFit.fill))),
           Text(program, style: context.bodySmall?.copyWith(fontSize: 12, fontWeight: FontWeight.w500, color: context.surface)),
-          5.height,
           Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 3,
             children: [
               Icon(Icons.place_rounded, size: 12, color: whiteColor),
-              3.width,
-              Text(venue, style: context.bodySmall?.copyWith(fontSize: 13, fontWeight: FontWeight.w600, color: context.tertiary)),
+              Text(venue, style: context.bodySmall?.copyWith(fontSize: 12, fontWeight: FontWeight.w600, color: context.tertiary)),
               Spacer(),
               Icon(Icons.schedule_rounded, size: 12, color: whiteColor),
-              3.width,
-              Text(time, style: context.bodySmall?.copyWith(fontSize: 13, fontWeight: FontWeight.w600, color: context.tertiary)),
+              Text(time, style: context.bodySmall?.copyWith(fontSize: 12, fontWeight: FontWeight.w600, color: context.tertiary)),
             ],
           )
         ],
