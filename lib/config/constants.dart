e7e81e49b5c0a4c57fbc40fa47cc6852f5f7c76e
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-01-08 00:04:53 +0530

    update 10 files and delete 2 files

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ConstanceData {
   static const String appName = '24th ICSD';
   static const String restorationScopeId = "24th_icsd.0.1";
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  static WidgetRef? notificationWidgetRef;
   static BuildContext? notificationContext;
 }
--- lib/view_model/riverpod.dart
+++
@@ -1,4 +0,0 @@
-import 'package:icsd/view_model/scaffold_vm.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final scaffoldVM = ChangeNotifierProvider<ScaffoldVM>((ref) => ScaffoldVM());
